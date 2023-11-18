26e82ff8b3e23ef47d2bf0c420aa9ff718a96fef
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-18 10:04:45 +0200

    xq

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,6 +2,14 @@ import styles from '../Home/Home.module.css';
 
 export default function Home() {
     return(
-        <p className={styles.homeText}>Home component here</p>
+        <div className={styles.section}>
+            {/* <div className={styles.imageBox}>
+            <img src={'/images/19366.jpg'} alt="Example" />
+            </div> */}
+            <h2 className={styles.title}>Example.com</h2>
+            <p className={styles.homeText}>Welcome to Example.com</p>
+            <p className={styles.homeText}>If you want Tech items you are on right spot here!</p>
+            <p className={styles.homeText}>Enjoy</p>
+        </div>
     )
 }
--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -3,6 +3,43 @@ import styles from '../Register/Register.module.css';
 
 export default function Register () {
     return (
-        <p className={styles.registerText}>Register Component</p>
+        <div className="form signup_form">
+            <form action="#">
+              <h2>Signup</h2>
+              <div className="input_box">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  required=""
+                />
+                <i className="uil uil-envelope-alt email" />
+              </div>
+              <div className="input_box">
+                <input
+                  type="password"
+                  placeholder="Create password"
+                  required=""
+                />
+                <i className="uil uil-lock password" />
+                <i className="uil uil-eye-slash pw_hide" />
+              </div>
+              <div className="input_box">
+                <input
+                  type="password"
+                  placeholder="Confirm password"
+                  required=""
+                />
+                <i className="uil uil-lock password" />
+                <i className="uil uil-eye-slash pw_hide" />
+              </div>
+              <button className="button">Signup Now</button>
+              <div className="login_signup">
+                Already have an account?{" "}
+                <a href="#" id="login">
+                  Login
+                </a>
+              </div>
+            </form>
+          </div>
     )
 }
