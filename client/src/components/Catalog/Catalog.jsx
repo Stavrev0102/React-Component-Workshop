e592dec74d3c3ec31037f6018586b07348e87e4a
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-19 13:48:56 +0200

    Add Store page

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,7 +1,37 @@
-import  style  from '../Catalog/Catalog.module.css'
+import styles  from '../Catalog/Catalog.module.css'
 
 export default function Catalog () {
     return (
-        <p className={style.catalogText}>Catalog component here</p>
+        <section  className={styles.dashboard}>
+        <h2>Store</h2>
+        <ul className={styles.cardWrapper}>
+          {/* Display a li with information about every post (if any)*/}
+          <li className={styles.card}>
+            <img src="https://s13emagst.akamaized.net/products/60458/60457155/images/res_4f32400ed4b4e50c33453c7b2d552785.jpg" alt="travis" />
+            <p>
+              <strong>Brand: </strong>
+              <span className="brand">Iphone</span>
+            </p>
+            <p>
+              <strong>Model: </strong>
+              <span className="model">14 Pro Max 128GB </span>
+            </p>
+            <p>
+              <strong>Color: </strong>
+              <span className="model">Grey</span>
+            </p>
+            <p>
+              <strong>Value:</strong>
+              <span className="value">2000</span>$
+            </p>
+            <a className={styles.detailsBtn} href="">
+              Details
+            </a>
+          </li>
+        </ul>
+        {/* Display an h2 if there are no posts */}
+        <h2>There are no items added yet.</h2>
+      </section>
+      
     )
 }
